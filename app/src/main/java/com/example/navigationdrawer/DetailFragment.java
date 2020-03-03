f7cb9237e65043ae337d29b677fb673612ccc1d1
monika kamboj` <monikakambojofficial96@gmail.com>
2020-03-03 19:22:48 +0530

    video player

--- app/src/main/java/com/example/navigationdrawer/DetailFragment.java
+++ app/src/main/java/com/example/navigationdrawer/DetailFragment.java
@@ -3,19 +3,33 @@ package com.example.navigationdrawer;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
 import java.io.Serializable;
 
 
 public class DetailFragment extends Fragment implements Serializable {
 
+    private VideoView videoView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    return inflater.inflate ( R.layout.activity_detail_fragment, container, false );
+        View rootView= inflater.inflate ( R.layout.activity_detail_fragment, container, false );
+        MediaController mc= new MediaController (getActivity());
+        videoView=rootView.findViewById(R.id.activity_detail_fragment_video_view );
+        String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.video;
+        videoView.setVideoURI( Uri.parse(path));
+        videoView.setMediaController(mc);
+        return rootView;
     }
 
 }
